4c506b17367edf0217fcc9745e4921c115a4e8b0
Bernabe <bernatattoo@gmail.com>
2023-08-16 16:15:44 -0300

    Add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,87 +1,91 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [show, setShow] = useState(false);
   return (
-    <div className=" top-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-neutral-900 dark:border-gray-600">
-      <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-        <button
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-        >
-          <svg
-            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-          </svg>
-          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400">
-            <Link to="/">Home</Link>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-        >
-          <svg
-            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
-            <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-            ></path>
-          </svg>
-          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400">
-            <Link to="/cotizaciones">Cotizaciones</Link>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-        >
-          <svg
-            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
-          </svg>
-          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400">
-            <Link to="/convertidor">Convertidor</Link>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-        >
-          <svg
-            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
-              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-            ></path>
-          </svg>
-          <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-cyan-400 dark:group-hover:text-cyan-400">
-            <Link to="/calculadora">Calculadora</Link>
-          </span>
-        </button>
-      </div>
-    </div>
+    <>
+      <nav className="border-gray-200 mb-10 px-8 py-4">
+        <div className="w-full mx-auto">
+          <div className="mx-2 flex flex-wrap items-center justify-between">
+            <Link to="/" className="flex">
+              <img src="public/favicon.png" alt="Icon" className="mx-4" />
+              <span className="self-center text-lg font-semibold whitespace-nowrap">
+                Dolar Hoy
+              </span>
+            </Link>
+
+            {/* Mobile Menu */}
+            {show ? (
+              <div className=" z-20 w-screen h-full bg-gray-700 rounded-md p-4">
+                <ul className="py-1" aria-labelledby="dropdown">
+                  <li>
+                    <Link
+                      to="/cotizaciones"
+                      onClick={() => setShow(!show)}
+                      className="text-sm hover:bg-gray-100 text-gray-500 block px-4 py-2"
+                    >
+                      Cotizaciones
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/convertidor"
+                      onClick={() => setShow(!show)}
+                      className="text-sm hover:bg-gray-100 text-gray-500 block px-4 py-2"
+                    >
+                      Convertidor
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/calculadora"
+                      className="text-sm hover:bg-gray-100 text-gray-500 block px-4 py-2"
+                      onClick={() => setShow(!show)}
+                    >
+                      Calculadora
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : null}
+            <div className="flex md:hidden md:order-2">
+              <button onClick={() => setShow(!show)}>menu</button>
+            </div>
+            <div
+              className="hidden md:flex justify-between items-end w-full md:w-auto md:order-1"
+              id="mobile-menu-3"
+            >
+              <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                  <Link
+                    to="/cotizaciones"
+                    className="bg-gray-500 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-gray-500 md:p-0 rounded"
+                  >
+                    Cotizaciones
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/convertidor"
+                    className="text-gray-500 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
+                  >
+                    Convertidor
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/calculadora"
+                    className="text-gray-500 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0"
+                  >
+                    Calculadora
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
